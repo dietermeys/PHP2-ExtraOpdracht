96bfa3b68e9ab340def99d6da6ec725f0a7aa13e
Dieter Meys <dietermeys@gmail.com>
2015-12-23 11:01:21 +0100

    Added some 'basic' bootstrap to login & register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,25 +1,44 @@
 @extends('master')
 
 @section('content')
-    {!!   Form::open(array('url' => 'login')) !!}
-    <h1>Login</h1>
+        <!-- if there are login errors, show them here -->
+@if (count($errors) > 0)
+    <div class="alert alert-danger margin">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+<div class="form-only">
+    <div class="col-md-6 col-md-offset-3">
+        <div class="form-container">
+            <h1 class="col-sm-offset-1">Login</h1>
+            {!! Form::open(array('url' => 'login')) !!}
 
-    <!-- if there are login errors, show them here -->
-    <p>
-        {!! $errors->first('email') !!}
-        {!! $errors->first('password') !!}
-    </p>
+            <div class="form-group">
+                <div class="col-sm-10 col-sm-offset-1">
+                    {!! Form::label('email', 'Email Address') !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control', 'placeholder' => 'Email']) !!}
+                </div>
+            </div>
 
-    <p>
-        {!! Form::label('email', 'Email Address') !!}
-        {!! Form::text('email', Input::old('email'), array('placeholder' => 'someone@somewhere.com')) !!}
-    </p>
+            <div class="form-group">
+                <div class="col-sm-10 col-sm-offset-1">
+                    {!! Form::label('password', 'Password') !!}
+                    {!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) !!}
+                </div>
+            </div>
 
-    <p>
-        {!! Form::label('password', 'Password') !!}
-        {!! Form::password('password') !!}
-    </p>
-
-    <p>{!! Form::submit('Submit!') !!}</p>
-    {!! Form::close() !!}
+            <div class="form-group">
+                <div class="col-sm-10 col-sm-offset-1">
+                    <br>
+                    <button class="btn btn-primary" type="submit">Login</button>
+                </div>
+            </div>
+            {!! Form::close() !!}
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     @if (count($errors) > 0)
-        <div class="alert alert-danger">
+        <div class="alert alert-danger margin">
             <ul>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
@@ -10,29 +10,49 @@
             </ul>
         </div>
     @endif
-    {!! Form::open(array('url' => 'register')) !!}
-    <div class="form-group">
-        {!! Form::label('name', 'Name') !!}
-        {!! Form::text('name', old('name'), ['class' => 'form-control', 'placeholder' => 'Name']) !!}
+    <div class="form-only">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="form-container">
+                <h1 class="col-sm-offset-1">Registration</h1>
+                {!! Form::open(array('url' => 'register')) !!}
+                <div class="form-group">
+                    <div class="col-sm-10 col-sm-offset-1">
+                        {!! Form::label('name', 'Name') !!}
+                        {!! Form::text('name', old('name'), ['class' => 'form-control', 'placeholder' => 'Name']) !!}
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-10 col-sm-offset-1">
+                        {!! Form::label('email', 'Email Address') !!}
+                        {!! Form::email('email', old('email'), ['class' => 'form-control', 'placeholder' => 'Email']) !!}
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-10 col-sm-offset-1">
+                        {!! Form::label('password', 'Password') !!}
+                        {!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) !!}
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-10 col-sm-offset-1">
+                        {!! Form::label('password_confirmation', 'Password confirmation') !!}
+                        {!! Form::password('password_confirmation', ['class' => 'form-control', 'placeholder' => 'Password Confirmation']) !!}
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-10 col-sm-offset-1">
+                        {!! Form::label('secret_code', 'Secret code') !!}
+                        {!! Form::text('secret_code', old('code'), ['class' => 'form-control', 'placeholder' => 'Secret code']) !!}
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-10 col-sm-offset-1">
+                        <br>
+                        <button class="btn btn-primary" type="submit">Register</button>
+                    </div>
+                </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
     </div>
-    <div class="form-group">
-        {!! Form::label('email', 'Email Address') !!}
-        {!! Form::email('email', old('email'), ['class' => 'form-control', 'placeholder' => 'Email']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('password', 'Password') !!}
-        {!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('password_confirmation', 'Password confirmation') !!}
-        {!! Form::password('password_confirmation', ['class' => 'form-control', 'placeholder' => 'Password Confirmation']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('secret_code', 'Secret code') !!}
-        {!! Form::text('secret_code', old('code'), ['class' => 'form-control', 'placeholder' => 'Secret code']) !!}
-    </div>
-    <div class="form-group button__margin">
-        <button class="btn btn-primary" type="submit">Register</button>
-    </div>
-    {!! Form::close() !!}
 @endsection
