2dcc957ea20e0d1066053b397412ea00b0a674d9
Dieter Meys <dietermeys@gmail.com>
2015-12-23 22:46:56 +0100

    Small style changes

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -12,7 +12,7 @@
     <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-<nav class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-custom navbar-fixed-top">
     <div class="container-fluid">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
